2dbd43224bbb17239ea07ba9481a8078f3f517b6
ray-zard <ray-zard@mail.ru>
2017-06-20 12:48:15 +0400

    Запили слайдер и навигацию к нему

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
 
   $('#slickCon').slick({
+    infinite: true,
+    autoplay: true,
+    speed: 300,
     arrows: false
   });
 
@@ -8,6 +11,14 @@ $(document).ready(function () {
     e.preventDefault();
     var slideno = $(this).data('slide');
     $('#slickCon').slick('slickGoTo', slideno - 1);
+    $('button[data-slide].active').removeClass('active');
+    $(this).toggleClass('active');
   });
+
+  $('#slickCon').on('setPosition', function() {
+    var currentSlide = $(this).slick('slickCurrentSlide');
+    $("button[data-slide]").removeClass("active");
+    $("button[data-slide]:nth-child("+(currentSlide + 1)+")").addClass("active");
+});
 
 });
